2f6fa9abff960d328c478a09238e1e4878cddb7c
kamlesh-67 <kamleshprajapat858@gmail.com>
2023-06-04 21:04:33 +0530

    first commit

---
+++ app/src/main/java/com/kamlesh/bpgpartner/manufacturers.java
@@ -0,0 +1,15 @@
+package com.kamlesh.bpgpartner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class manufacturers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manufacturers);
+        getSupportActionBar().hide();
+    }
+}
