19abbb7e278e63208a26eacb741ba2100ae94aaf
kamlesh-67 <kamleshprajapat858@gmail.com>
2023-06-08 00:02:12 +0530

    all design are completed today, tomorrow code will be started

---
+++ app/src/main/java/com/kamlesh/bpgpartner/formula.java
@@ -0,0 +1,15 @@
+package com.kamlesh.bpgpartner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class formula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formula);
+        getSupportActionBar().hide();
+    }
+}
